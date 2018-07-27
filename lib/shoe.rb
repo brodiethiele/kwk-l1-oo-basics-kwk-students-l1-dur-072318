076b71bb78b44f83b509be2d0ea02102473496d1
brodiethiele <brodie.thiele@gmail.com>
2018-07-27 12:59:14 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+class Shoe
+  
+attr_accessor: :color, :size, :material, :condition
+attr_reader: :brand
+  def initialize(brand)
+  @brand=brand
+  end
+  # def color=(color)
+  #   @color=color
+  # end 
+  # def size=(size)
+  #   @size=size
+  # end 
+  # def material=(material)
+  #   @material=material
+  # end
+  # def condition=(condition)
+  #   @condition=condition
+  # end 
+
+end
+shoe1=Shoe.new("Adidas")
